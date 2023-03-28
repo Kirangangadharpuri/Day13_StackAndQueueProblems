5e28dde72a619269cadc0663fb154a96b239fd02
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-28 02:28:37 -1200

    Welcome page

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedListAsmnt
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Stack Problem");
+        }
+    }
+}
