3b8f82f4e73adedbfba464e1e0511689a55c0573
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-28 03:18:38 -1200

    Inserting and deleting values in stack

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Stack Problem");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(56);
+            linkedListStack.Push(30);
+            linkedListStack.Push(70);
+            linkedListStack.Display();
+            linkedListStack.Peek();
+            linkedListStack.Pop();
+            linkedListStack.Display();
+            linkedListStack.Peek();
+
         }
     }
 }
