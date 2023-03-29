c3046dec9f27721b4cf1a7fc070a347cd82792ad
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-28 21:35:02 -1200

    Inserting and deletion values in queue

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Stack Problem");
+            LinkedListQueue obj = new LinkedListQueue();
+            obj.Enqueue(56);
+            obj.Enqueue(30);
+            obj.Enqueue(70);
+            Console.WriteLine("Element stored in queue is:-");
+            obj.Display();
+            Console.WriteLine("After Dequeue operation:");
+            obj.Dequeue();
+            Console.WriteLine("Element stored in queue is:-");
+            obj.Display();
         }
     }
 }
