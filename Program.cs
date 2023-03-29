95d91ea150165d972aa9db9b6cf4e36877b8d184
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-28 21:28:01 -1200

    Inserting values in queue

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Stack Problem");
+            LinkedListQueue obj = new LinkedListQueue();
+            obj.Enqueue(56);
+            obj.Enqueue(30);
+            obj.Enqueue(70);
+            obj.Display();
         }
     }
 }
