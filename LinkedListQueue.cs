0392304578e1920a45241c6887c2453fd46bf31e
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-28 21:35:45 -1200

    create code for queue

---
+++ LinkedListQueue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedListAsmnt
+{
+    public class LinkedListQueue
+    {
+        public Node head = null;
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine($"{node.data} inserted into Queue");
+        }
+        public void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+        public void Dequeue()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is empty,Dequeue is not possible.");
+                return;
+            }
+            while (head != null)
+            {
+                Console.WriteLine($"Value dequeue is {head.data}");
+                head = head.next;
+            }
+        }
+    }
+}
